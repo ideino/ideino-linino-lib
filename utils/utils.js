4ff772dbdbf9551754170c712e1c830044aa8cd7
quasto <sergio.tomasello@gmail.com>
2014-03-07 18:22:52 +0100

    fix: delete not used var

--- utils/utils.js
+++ utils/utils.js
@@ -2,8 +2,6 @@ var modes = { INPUT : 'input', OUTPUT : 'output', PWM: 'pwm', SERVO: 'servo'},
 	hi = 1,
 	lo = 0;
 
-
 exports.MODES = modes;
 exports.HIGH = hi;
 exports.LOW = lo;
-exports.PARAMS = params;
